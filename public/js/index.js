96cc2f909240ab5f9119e4e12334971e194dfb3e
AlyssaT03 <amtraylor03@gmail.com>
2021-08-04 23:26:20 +0000

    order by date works maybe

--- public/js/index.js
+++ public/js/index.js
@@ -2,16 +2,16 @@ let googleUser;
 let eventType;
 
 window.onload = event => {
-  // Use this to retain user state between html pages.
-  firebase.auth().onAuthStateChanged(function(user) {
-    if (user) {
-      console.log("Logged in as: " + user.displayName);
-      googleUser = user;
-        getNotes();
-    } else {
-      window.location = "signIn.html"; // If not logged in, navigate back to login page.
-    }
-  });
+    // Use this to retain user state between html pages.
+    firebase.auth().onAuthStateChanged(function(user) {
+        if (user) {
+            console.log("Logged in as: " + user.displayName);
+            googleUser = user;
+            getNotes();
+        } else {
+            window.location = "signIn.html"; // If not logged in, navigate back to login page.
+        }
+    });
 };
 
 const addEntry = document.querySelector("#addEntry");
@@ -19,96 +19,92 @@ const modal = document.querySelector("#modalElement");
 const dropDown = document.querySelector(".dropdown-menu");
 
 addEntry.addEventListener("click", (e) => {
- modal.classList.add("is-active");
- 
- 
+    modal.classList.add("is-active");
+
+
 });
- 
+
 const saveButton = document.querySelector("#save");
 const cancelButton = document.querySelector("#cancel");
 
 console.log(saveButton);
 
 saveButton.addEventListener("click", e => {
-  modal.classList.remove("is-active");
-  console.log("saved");
- getData()
+    modal.classList.remove("is-active");
+    console.log("saved");
+    getData()
 
 })
 
 cancelButton.addEventListener("click", e => {
-  modal.classList.remove("is-active");
-  console.log("canceled");
+    modal.classList.remove("is-active");
+    console.log("canceled");
 })
 
-function getData(){
-     var name = document.querySelector("#eventName").value;
+function getData() {
+    var name = document.querySelector("#eventName").value;
     var date = document.querySelector("#date").value;
-    var description = document.querySelector("#description").value; 
-    var sport = document.querySelector("#sport").value; 
+    var description = document.querySelector("#description").value;
+    var sport = document.querySelector("#sport").value;
     var eventType = document.querySelector("#eventType").value;
 
-firebase.database().ref(`${googleUser.uid}/Events`).push({
-    Name: name,
-    Date: date,
-    "EventType":eventType,
-    "Sport":sport,
-    Description: description,
+    firebase.database().ref(`${googleUser.uid}/Events`).push({
+        Name: name,
+        Date: date,
+        "EventType": eventType,
+        "Sport": sport,
+        Description: description,
 
-  })
-
-   getNotes();
-
+    })
+    getNotes();
 }
 
-
 const getNotes = userId => {
-  const eventsRef = firebase.database().ref(`${googleUser.uid}/Events`)
-  eventsRef.orderByChild("title").on("value", snapshot => {
-    renderDataAsHtml(snapshot);
-  });
+    const eventsRef = firebase.database().ref(`${googleUser.uid}/Events`)
+    eventsRef.orderByChild("title").on("value", snapshot => {
+        renderDataAsHtml(snapshot);
+    });
 };
 
 //Given a list of notes, render them in HTML
 const renderDataAsHtml = (data) => {
-  let cards = "";
-  data.forEach((child) => {
-    const event = child.val();
-    const eventItem = child.key
-    cards += createCard(event, eventItem);
+    let eventList = [];
+     data.forEach((child) => {
+        const childObj = child.val();
+        childObj.id = child.key;
+        eventList.push(childObj)
+     })
+     console.log(eventList);
+    const sortedData = eventList.sort(function(a,b){
+        d1 = new Date(a.Date).getTime();
+        d2 = new Date(b.Date).getTime();
+        return d1 - d2});
+        console.log(sortedData);
+    let cards = "";
+    sortedData.forEach((child) => {
+        const event = child;
+        const eventItem = child.id;
+        cards += createCard(event, eventItem);
+    })
 
-  })
-
-  document.querySelector("#app").innerHTML = cards;
+    document.querySelector("#app").innerHTML = cards;
 };
 
-
-
-
-function deleteNote(eventItem)
-{
-  
- 
+function deleteNote(eventItem) {
     firebase.database().ref(`${googleUser.uid}/Events/${eventItem}`).remove();
-    
- 
 }
-
-
-
 
 // Return a note object converted into an HTML card
 const createCard = (event, eventItem) => {
-  const colors = ["has-background-primary-light", "has-background-link-light", "has-background-info-light", "has-background-success-light", "has-background-warning-light", "has-background-danger-light","has-background-primary-dark","has-background-link-dark","has-background-info-dark","has-background-success-dark","has-background-warning-dark","has-background-danger-dark"]
-  var bk_color = colors[Math.floor(Math.random()*colors.length)]
-  if(event.EventType=="practice"){
-      bk_color = colors[0]
-  }
-  else if(event.EventType=="tournament"){
-      bk_color = colors[4]
-  }
-  
-  return `
+    const colors = ["has-background-primary-light", "has-background-link-light", "has-background-info-light", "has-background-success-light", "has-background-warning-light", "has-background-danger-light", "has-background-primary-dark", "has-background-link-dark", "has-background-info-dark", "has-background-success-dark", "has-background-warning-dark", "has-background-danger-dark"]
+    var bk_color = colors[Math.floor(Math.random() * colors.length)]
+    if (event.EventType == "practice") {
+        bk_color = colors[0]
+    } else if (event.EventType == "tournament") {
+        bk_color = colors[4]
+    }
+
+    return `
          <div class="column is-one-quarter">
          <div class="card ${bk_color}">
            <header class="card-header">
@@ -132,7 +128,8 @@ const createCard = (event, eventItem) => {
          </div>
        </div> `;
 };
-  function changeEventType(selectButton){
+
+function changeEventType(selectButton) {
     eventType = selectButton.target.value;
     console.log("Event type changed");
-  }
+}
